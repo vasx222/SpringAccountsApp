8448784986e870de10ac241ca417601e475facf4
vasx222 <vasx222@mail.ru>
2018-04-04 23:25:26 +0300

    fixed

--- spring_account/src/main/java/ru/liga/mock/AccountDaoMock.java
+++ spring_account/src/main/java/ru/liga/mock/AccountDaoMock.java
@@ -1,13 +1,14 @@
 package ru.liga.mock;
 
+import org.springframework.stereotype.Component;
+import ru.liga.dao.AccountDao;
 import ru.liga.domain.Account;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class AccountDaoMock {
+@Component
+public class AccountDaoMock implements AccountDao {
 
     private List<Account> accounts = new ArrayList<>();
     public AccountDaoMock() {
@@ -15,7 +16,20 @@ public class AccountDaoMock {
         accounts.add(new Account(2L, "Bert", 2L, 155.0));
         accounts.add(new Account(3L, "Bob", 1L, 120.0));
     }
-    public List<Account> getAccounts() {
+    @Override
+    public List<Account> getAll() {
         return accounts;
+    }
+    @Override
+    public List<Account> getById(Long id) {
+        return accounts.stream()
+                .filter(account -> account.getId().equals(id))
+                .collect(Collectors.toList());
+    }
+    @Override
+    public List<Account> getByOwner(Long id) {
+        return accounts.stream()
+                .filter(account -> Objects.equals(account.getOwner(), id))
+                .collect(Collectors.toList());
     }
 }
--- spring_account/src/main/java/ru/liga/service/AccountService.java
+++ spring_account/src/main/java/ru/liga/service/AccountService.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Component;
 import ru.liga.dao.AccountDao;
 import ru.liga.domain.Account;
 
-import java.util.List;
-
 @Component
 public class AccountService {
 
@@ -14,34 +12,21 @@ public class AccountService {
     private AccountDao accountDao;
 
     public void printAll() {
-        for (Account account : accountDao.getAll()) {
-            System.out.println(account);
-        }
+        accountDao.getAll().forEach(System.out::println);
     }
     public void multiplyAll(Double multiplier) {
-        for (Account account : accountDao.getAll()) {
-            account.setVolume(account.getVolume() * multiplier);
-        }
+        accountDao.getAll().forEach(account ->
+                account.setVolume(account.getVolume() * multiplier));
     }
     public void multiplyOwner(Long ownerId, Double multiplier) {
-        List<Account> accounts = accountDao.getByOwner(ownerId);
-        for (Account account : accounts) {
-            account.setVolume(account.getVolume() * multiplier);
-        }
+        accountDao.getByOwner(ownerId).forEach(account ->
+                account.setVolume(account.getVolume() * multiplier));
     }
     public Double getTotalVolume() {
-        Double totalVolume = 0.0;
-        for (Account account : accountDao.getAll()) {
-            totalVolume += account.getVolume();
-        }
-        return totalVolume;
+        return accountDao.getAll().stream().mapToDouble(Account::getVolume).sum();
     }
     public Double getTotalOwnerVolume(Long ownerId) {
-        List<Account> accounts = accountDao.getByOwner(ownerId);
-        Double totalVolume = 0.0;
-        for (Account account : accounts) {
-            totalVolume += account.getVolume();
-        }
-        return totalVolume;
+        return accountDao.getByOwner(ownerId).stream()
+                .mapToDouble(Account::getVolume).sum();
     }
 }
